4f9072df4881f89d06ad82f9002f7fc4b33500ab
Hadiroudbari <hadiroudbari@gmail.com>
2023-12-07 22:15:22 +0330

    Refactor some files

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
-import GlobalStyles from "./styles/GlobalStyles";
 
-import AppLayout from "./ui/AppLayout";
-import Home from "./pages/ShopHome";
-import Shop from "./pages/ShopProducts";
-import Orders from "./pages/ShopOrders";
-import Cart from "./pages/ShopCart";
-import About from "./pages/ShopAbout";
-import Contact from "./pages/ShopContact";
-import Checkout from "./pages/ShopCheckout";
-import ProductDetails from "./pages/ShopProductDetails";
-import Profile from "./pages/CustomerProfile";
-import PageNotFound from "./pages/PageNotFound";
-import ProtectedRoute from "./ui/ProtectedRoute";
-import AdminLayout from "./ui/AdminLayout";
-import AdminDashboard from "./pages/AdminDashboard";
-import AdminProducts from "./pages/AdminProducts";
-import AdminCategories from "./pages/AdminCategories";
-import AdminOrders from "./pages/AdminOrders";
-import AdminOrderDetails from "./pages/AdminOrderDetails";
-import AdminUsers from "./pages/AdminUsers";
-import AdminSettings from "./pages/AdminSettings";
-import AdminLogin from "./pages/AdminLogin";
+import GlobalStyles from "./styles/GlobalStyles";
+import Router from "./ui/Router";
+import ToasterConfig from "./ui/Toaster";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -39,64 +18,10 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="home" />} />
-            <Route path="home" element={<Home />} />
-            <Route path="shop" element={<Shop />} />
-            <Route path="shop/:productId" element={<ProductDetails />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="orders" element={<Orders />} />
-            <Route path="checkout" element={<Checkout />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="profile" element={<Profile />} />
-          </Route>
-          <Route
-            path="admin"
-            element={
-              <ProtectedRoute>
-                <AdminLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Navigate replace to="dashboard" />} />
-            <Route path="dashboard" element={<AdminDashboard />} />
-            <Route path="products" element={<AdminProducts />} />
-            <Route path="orders" element={<AdminOrders />} />
-            <Route path="orders/:orderId" element={<AdminOrderDetails />} />
-            <Route path="categories" element={<AdminCategories />} />
-            <Route path="users" element={<AdminUsers />} />
-            <Route path="settings" element={<AdminSettings />} />
-          </Route>
-          <Route path="login" element={<AdminLogin />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
 
-      <Toaster
-        position="top-center"
-        gutter={12}
-        containerStyle={{
-          margin: "8px",
-        }}
-        toastOptions={{
-          success: {
-            duration: 3000,
-          },
-          error: {
-            duration: 5000,
-          },
-          style: {
-            fontSize: "16px",
-            maxWidth: "500px",
-            padding: "16px 24px",
-            backgroundColor: "var(--color-grey-0)",
-            color: "var(--color-grey-700)",
-          },
-        }}
-      />
+      <Router />
+
+      <ToasterConfig />
     </QueryClientProvider>
   );
 }
