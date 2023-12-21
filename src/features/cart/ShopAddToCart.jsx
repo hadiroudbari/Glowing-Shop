103ee16348d0060486f60a427c304a7f9b846016
Hadiroudbari <hadiroudbari@gmail.com>
2023-12-21 16:32:03 +0330

    Add Cart to LocalStorage

--- src/features/cart/ShopAddToCart.jsx
+++ src/features/cart/ShopAddToCart.jsx
@@ -1,10 +1,27 @@
+import { useLocalStorageState } from "../../hooks/useLocalStorageState";
 import Button from "../../ui/Button";
 
-function AddToCart({ stock }) {
+function AddToCart({ product, quantity }) {
+  const [cart, setCart] = useLocalStorageState([], "cart");
+
+  const { id: productId, stock } = product;
+
+  function handleAddToCart(product) {
+    const currentItem = cart.find((item) => item.id === productId);
+
+    if (currentItem)
+      setCart((prev) =>
+        prev.map((item) =>
+          item.id === currentItem.id ? { ...item, quantity } : item
+        )
+      );
+    else setCart((prev) => [...prev, { ...product, quantity }]);
+  }
+
   return (
     <>
       {stock ? (
-        <Button bg="dark" size="large">
+        <Button bg="dark" size="large" onClick={() => handleAddToCart(product)}>
           Add to Bag
         </Button>
       ) : (
