8beea7589d62ab1faf869297f20a54c1f4bfd9f3
Hadiroudbari <hadiroudbari@gmail.com>
2023-12-24 09:26:19 +0330

    Refactor Cart Functions

--- src/services/apiCart.js
+++ src/services/apiCart.js
@@ -3,3 +3,37 @@ export function getCart() {
 
   return cart;
 }
+
+export function addToCart(product, quantity) {
+  const cart = getCart() || [];
+
+  const currentItem = cart.find((item) => item.id === product.id);
+
+  if (currentItem)
+    localStorage.setItem(
+      "cart",
+      JSON.stringify(
+        cart.map((item) =>
+          item.id === currentItem.id ? { ...item, quantity } : item
+        )
+      )
+    );
+  else
+    localStorage.setItem(
+      "cart",
+      JSON.stringify([...cart, { ...product, quantity }])
+    );
+
+  return cart;
+}
+
+export function deleteFromCart(id) {
+  const cart = getCart();
+
+  localStorage.setItem(
+    "cart",
+    JSON.stringify(cart.filter((item) => item.id !== id))
+  );
+
+  return cart;
+}
